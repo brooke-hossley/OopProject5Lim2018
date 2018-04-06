2dd4a8eadf3b2ed3729eaf9c32cef678fc11a1ec
Patrick Barber <37307782+patrick-barber@users.noreply.github.com>
2018-04-06 12:28:12 -0400

    Update Route.java

--- Route.java
+++ Route.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 ////////////////////////////////////////////////////////////////////////////////
 /**
  * Write a description of class Route here.
@@ -11,13 +12,70 @@ public class Route
     protected RouteColor color; //The color of the route
     protected boolean doubleTrack;//If the route has double or triple capacity
     protected boolean tripleTrack;
-
+    private ArrayList<String> cities = new ArrayList<String>();
+    private ArrayList<RouteColor> colors = new ArrayList<RouteColor>();
     public Route(int length, RouteColor color, boolean doubleTrack, 
     boolean tripleTrack){
         this.length = length;
         this.color = color;
         this.doubleTrack = doubleTrack;
         this.tripleTrack = tripleTrack;
+    }
+
+    public Route(String firstCity, String secondCity)
+    {
+        //cities.add(firstCity);
+        //cities.add(secondCity);
+        String cityOneName = firstCity;
+        String cityTwoName = secondCity;
+        if (cityOneName.equals("Hamburg"))
+        {
+            if(cityTwoName.equals("Rostock"))
+            {
+                //set length
+                //set the color to whatever its supposed to be 
+                //colors.add(RouteColor.YELLOW);
+                //set points?
+            }
+            else if (cityTwoName.equals("Berlin"))
+            {
+                //set length
+                //set the color to whatever its supposed to be 
+                //colors.add(RouteColor.YELLOW);
+            }
+            else if(cityTwoName.equals(""))
+            {
+
+            }
+        }
+        else if(cityOneName.equals("Berlin"))
+        {
+            //continue on with whats above for every city and their possible routes
+        }
+    }
+
+    public static String getStringrouteColor(RouteColor col) {
+        if(col == YELLOW)
+        {
+            return "yellow";
+        }
+        else if( col == ORANGE)
+        {
+            return "orange";
+        }
+        return "orange";
+        return "green";
+        return "blue";
+        return "red";
+        return "gray";
+        return "white";
+        return "black";
+        return "purple";
+        //else
+        {
+            return "nosuchcolor";
+        }
+        
     }
 
     protected int getRouteLength(){
@@ -31,8 +89,8 @@ public class Route
     protected boolean isDouble(){
         return doubleTrack;   
     }
-    
-     protected boolean isTriple(){
+
+    protected boolean isTriple(){
         return tripleTrack;   
     }
 }
