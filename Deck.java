23c4c4cdc79a2e140895a459e38ab1a86c5a3c40
Patrick Barber <37307782+patrick-barber@users.noreply.github.com>
2018-04-06 12:27:48 -0400

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -8,13 +8,16 @@ import java.util.Random;
  */
 public class Deck
 {
-   protected ArrayList<Card> cards = new ArrayList<>();
-    protected Random r = new Random();
+    protected ArrayList<Card> cards = new ArrayList<>();
+    protected Random ran = new Random();
 
     /**
      * Empty constructor for objects of class Deck
      */
-    public Deck() {};
+    public Deck() 
+    {
+
+    }
 
     /**
      * Constructor for objects of class Deck
@@ -22,25 +25,27 @@ public class Deck
      */
     public Deck(ArrayList<Card> c)
     {
-        for (Card cr : c)
-            cards.add(cr);
+        for (Card cd : c)
+        {
+            cards.add(cd);
+        }
     }
-    
+
     /**
      * Draws a Card from this.cards
      * @return the randomly-drawn Card
      */
     public Card drawCard() {
-        int i = r.nextInt(cards.size());
+        int i = ran.nextInt(cards.size());
         return cards.remove(i);
     }
-    
+
     /**
      * Adds a Card to this.cards in a random location
      * @param c Card to add to this.cards
      */
     public void addCard(Card c) {
-        int i = r.nextInt(cards.size() + 1);
+        int i = ran.nextInt(cards.size() + 1);
         cards.add(i, c);
     }
 }
