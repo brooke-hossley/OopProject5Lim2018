d16619676e285b563c9d9f5b0ff964f825bf66b6
brooke-hossley <31697381+brooke-hossley@users.noreply.github.com>
2018-04-19 19:19:59 -0400

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.event.*;
 import java.awt.image.*;
+import java.awt.font.*;
 import java.io.*;
 /**
  * The class that runs the game with a level 
@@ -15,12 +16,13 @@ public class Driver extends JPanel implements ActionListener
 {
     //background image
     private static Image background, background2;
+    private Image train;
     //JButtons for Player selection and rules
     private JButton twoPlayers;
     private JButton threePlayers;
     private JButton rules;
     //Player ArrayList
-    private ArrayList<Player> list;
+    private ArrayList<Player> list = new ArrayList<Player>();;
 
     //The frame to display the menu on
     protected static JFrame frame;
@@ -30,13 +32,13 @@ public class Driver extends JPanel implements ActionListener
     public Driver()
     {
         //Images with paths
-        //String dir = "Images\\";
-        //background = new ImageIcon(dir + "HomeScreen.jpg").getImage();
-        //background = background.getScaledInstance();
-        background2 = new ImageIcon("Images" + File.separator + "homescreenbackground.jpg").getImage();
-        //background2 = background2.getScaledInstance(1200, 700, Image.SCALE_DEFAULT);
+        String dir = "Images\\";
+        background = new ImageIcon(dir + "HomeScreen.jpg").getImage();
+        background2 = new ImageIcon(dir + "homescreenbackground.jpg").getImage();
+        train = new ImageIcon(dir + "Traingif.gif").getImage();
+        train = train.getScaledInstance(500, 300, Image.SCALE_DEFAULT);
         //Set window to correct size
-        Dimension size = new Dimension(1200, 700);
+        Dimension size = new Dimension(1500, 800);
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
@@ -45,30 +47,30 @@ public class Driver extends JPanel implements ActionListener
         //Creating the JButtons with actionListeners
         twoPlayers = new JButton("2");
         twoPlayers.setFont(new Font("Arial", Font.BOLD, 40));
-        twoPlayers.setBackground(Color.RED);
-        twoPlayers.setBounds(750,300,200,100);
+        twoPlayers.setBackground(Color.CYAN);
+        twoPlayers.setBorder(BorderFactory.createLineBorder(Color.black));
+        twoPlayers.setBounds(400,615,100,75);
         add(twoPlayers);
         twoPlayers.addActionListener(this);
         twoPlayers.setActionCommand("Two");
-        add(twoPlayers);
 
         threePlayers = new JButton("3");
-        threePlayers.setFont(new Font("Arial", Font.BOLD, 20));
+        threePlayers.setFont(new Font("Arial", Font.BOLD, 40));
         threePlayers.setBackground(new Color(51,153,255));
-        threePlayers.setBounds(100,600,100,50);
+        threePlayers.setBorder(BorderFactory.createLineBorder(Color.black));
+        threePlayers.setBounds(600,615,100,75);
         add(threePlayers);
         threePlayers.addActionListener(this);
         threePlayers.setActionCommand("Three");
-        add(threePlayers);
 
         rules = new JButton("Rules");
         rules.setFont(new Font("Arial", Font.BOLD, 20));
-        rules.setBackground(new Color(51,153,255));
-        rules.setBounds(100,600,100,50);
+        rules.setBackground(Color.RED);
+        rules.setBorder(BorderFactory.createLineBorder(Color.black));
+        rules.setBounds(850,630,100,50);
         add(rules);
         rules.addActionListener(this);
         rules.setActionCommand("Rules");
-        add(rules);
     }
 
     /**
@@ -81,6 +83,41 @@ public class Driver extends JPanel implements ActionListener
         super.paintComponent(g);
         g.drawImage(background2, 0, 0, null);
         g.drawImage(background, 0, 0, null);
+        g.drawImage(train, 1000, 500, this);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 80));
+        String text = "Germany";
+        int x = 600; int y = 490;
+        g.drawString(text, ShiftWest(x, 1), ShiftNorth(y, 1));
+        g.drawString(text, ShiftWest(x, 1), ShiftSouth(y, 1));
+        g.drawString(text, ShiftEast(x, 1), ShiftNorth(y, 1));
+        g.drawString(text, ShiftEast(x, 1), ShiftSouth(y, 1));
+        g.setColor(new Color(204, 0, 0));
+        g.drawString(text, x, y);
+        String text2A = "Select the number";
+        String text2B = "of players to begin!";
+        g.setColor(Color.WHITE);
+        g.fillRect(50, 600, 300, 100);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.setColor(Color.BLACK);
+        g.drawString(text2A, 70, 650);
+        g.drawString(text2B, 60, 680);
+    }
+
+    int ShiftNorth(int p, int distance) {
+        return (p - distance);
+    }
+
+    int ShiftSouth(int p, int distance) {
+        return (p + distance);
+    }
+
+    int ShiftEast(int p, int distance) {
+        return (p + distance);
+    }
+
+    int ShiftWest(int p, int distance) {
+        return (p - distance);
     }
 
     /**
@@ -91,11 +128,11 @@ public class Driver extends JPanel implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         String action = e.getActionCommand();
-        int count = 0;
+        int count = 1;
         if(action.equals("Two")) 
         {
             PlayGame.numberOfPlayers = 2;
-            while(count < 2){
+            while(count < 3){
                 String name = JOptionPane.showInputDialog(frame, "Enter in Player " + count + 
                         " name:");
                 String color = JOptionPane.showInputDialog(frame, "Enter in Player " + count + 
@@ -110,7 +147,7 @@ public class Driver extends JPanel implements ActionListener
         else if(action.equals("Three"))
         {
             PlayGame.numberOfPlayers = 3;
-            while(count < 3){
+            while(count < 4){
                 String name = JOptionPane.showInputDialog(frame, "Enter in Player " + count + 
                         " name:");
                 String color = JOptionPane.showInputDialog(frame, "Enter in Player " + count + 
@@ -127,6 +164,7 @@ public class Driver extends JPanel implements ActionListener
 
         }
     }
+    //Make a getPlayers() to return arraylist
 
     /**
      * Creates the JFrame for the menu
