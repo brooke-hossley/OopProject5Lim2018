00cb4991de9a2e7b2957fbc91b35252979eba5f5
Patrick Barber <37307782+patrick-barber@users.noreply.github.com>
2018-04-23 11:57:42 -0400

    Update DestinationCardPanel.java

--- DestinationCardPanel.java
+++ DestinationCardPanel.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
+import java.util.*;
 /**
  * Write a description of class DestinationCardPanel here.
  *
@@ -20,7 +21,7 @@ public class DestinationCardPanel extends JPanel
         setSize(size);
         setLayout(null);
     }
-    
+
     protected static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("Your Destination Tickets");
@@ -31,14 +32,24 @@ public class DestinationCardPanel extends JPanel
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     public void paintComponent(Graphics g) 
     {
         super.paintComponent(g);
         g.drawImage(woodBackground,0,0,null);
         //draw the players destination tickets here
-        
+        ArrayList<DestinationCard> playersDestinationCards = new ArrayList(); 
+        playersDestinationCards = PlayGame.currentPlayer.getDestinationCards();
+        int x = 20;
+        int y = 20;
+        for(int i = 0; i< playersDestinationCards.size();i++)
+        {
+            g.drawImage(playersDestinationCards.get(i).getPicture(),x,y,null);
+            x+=100;
+            y+=100;
+        }
     }
+    
 
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
