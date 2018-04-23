535f5ac74d6a6624ad448d23f562db0507c55987
Patrick Barber <37307782+patrick-barber@users.noreply.github.com>
2018-04-23 11:58:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -49,7 +49,7 @@ public class Player
         else if (pColor.equalsIgnoreCase("Black"))
             color = new Color(50, 50, 50);
     }
-    
+
     protected boolean useTrainCars(int count) {
         if (count <= carsRemaining) {
             carsRemaining -= count;
@@ -78,20 +78,20 @@ public class Player
             n = 15;
         else if (route.length == 7) 
             n = 18;
-            
+
         score += n;
         useTrainCars(route.length);
         route.occupiedByColor = this.color;
         controlledRoutes.add(route);
     }
-    
+
     //checks if player can legally take route if it's available
     //by determining if player has enough train pieces and correct train cards
     protected boolean canTakeRoute(Route route) {
         if (!route.isOpen() || carsRemaining < route.length) {
             return false;
         }
-        
+
         if (route.color.contains(RouteColor.GRAY)) {
             for (int i=0; i<8; i++) {
                 if (trainCounts[8] + trainCounts[i] >= route.length)
@@ -99,47 +99,47 @@ public class Player
             }
             return false;
         }
-        
+
         if (route.color.contains(RouteColor.YELLOW)) {
             if (trainCounts[8] + trainCounts[0] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.BLUE)) {
             if (trainCounts[8] + trainCounts[1] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.GREEN)) {
             if (trainCounts[8] + trainCounts[2] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.PINK)) {
             if (trainCounts[8] + trainCounts[3] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.RED)) {
             if (trainCounts[8] + trainCounts[4] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.BLACK)) {
             if (trainCounts[8] + trainCounts[5] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.ORANGE)) {
             if (trainCounts[8] + trainCounts[6] >= route.length)
                 return true;
         }
-        
+
         if (route.color.contains(RouteColor.WHITE)) {
             if (trainCounts[8] + trainCounts[7] >= route.length)
                 return true;
         }
-        
+
         return false;
     }
 
@@ -163,7 +163,7 @@ public class Player
     protected void addDestinationCard(DestinationCard d){
         destinations.add(d);
     }
-    
+
     protected void CompleteDestinationCard(DestinationCard d){
         if(destinations.contains(d)){
             destinations.remove(d);
@@ -171,7 +171,7 @@ public class Player
         //score += d.getPoints();
         completedDestinations.add(d);
     }
-    
+
     //finds score to add based on all the completed destination cards
     protected int getPosDestScore() {
         int total = 0;
@@ -180,7 +180,7 @@ public class Player
         }
         return total;
     }
-    
+
     //finds score to subtract based on all the incompleted destination cards
     protected int getNegDestScore() {
         int total = 0;
@@ -189,4 +189,9 @@ public class Player
         }
         return total;
     }
+
+    public ArrayList<DestinationCard> getDestinationCards()
+    {
+        return destinations;
+    }   
 }
